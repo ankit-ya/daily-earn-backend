658753822a9f27b0523f9434ed0d556b0706116c
ankit ya <ankit3001yadav@gmail.com>
2025-02-06 13:09:36 +0530

    socket fix

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
